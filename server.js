9b28a7535991fc1ec4815a4f80c5ccf759a83180
SAMUEL ALHADEF <145781410+Samuelalhadef@users.noreply.github.com>
2024-12-13 13:44:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // Configuration Spotify
-const CLIENT_ID = '1e5be8f0746c49e5a7a6ae3efe30d28b';
-const CLIENT_SECRET = '7de0421ed068446fa699cbf130033982';
+const CLIENT_ID = 'Client_ID';
+const CLIENT_SECRET = 'Client_secret';
 let spotifyToken = null;
 let tokenExpirationTime = null;
 
